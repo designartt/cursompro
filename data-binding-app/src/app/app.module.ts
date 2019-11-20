d0138deaf818c4949a24e4d1b07a210d70bc7cde
designartt <designarttdev@gmail.com>
2019-11-20 14:06:25 -0300

    Primeiro commit Curso Angular MPro

---
+++ data-binding-app/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StringInterpolationComponent } from './string-interpolation/string-interpolation.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StringInterpolationComponent,
+    PropertyBindingComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
